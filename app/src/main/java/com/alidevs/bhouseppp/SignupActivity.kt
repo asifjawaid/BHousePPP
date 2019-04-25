7f9cd4853d99f78280189897f1c5433bbbe082f5
aasif1297 <aasif1297@gmail.com>
2019-04-25 11:36:57 +0500

    Initial commit

---
+++ app/src/main/java/com/alidevs/bhouseppp/SignupActivity.kt
@@ -0,0 +1,13 @@
+package com.alidevs.bhouseppp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SignupActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
